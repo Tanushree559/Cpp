59ab5e9edc6842d0dcd774ad0b985d08dfc9f55e
Tanushree Paul <133580914+Tanushree559@users.noreply.github.com>
2024-07-04 14:23:13 +0530

    Add files via upload

---
+++ data_modifiers.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    short int si;
+    long int li;
+    cout<<"sizeof short int"<<sizeof(si)<<endl;
+    cout<<"sizeof long int"<<sizeof(li)<<endl;
+    return(0);
+}
