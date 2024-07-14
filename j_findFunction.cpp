b3efef242085b2ee1cae5eb724fafc890d553295
Tanushree Paul <133580914+Tanushree559@users.noreply.github.com>
2024-07-14 23:33:07 +0530

    Add files via upload

---
+++ j_findFunction.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string s1="nincompoop";
+    cout<<s1.find("poo")<<endl;
+    return(0);
+}
---
+++ j_upperLowerCase.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    string str="adsfjhalksdjf";
+    //convert into upper case
+    for(int i=0;i<str.size();i++)
+    {
+        if(str[i]>='a'&&str[i]<='z')
+        {
+            str[i]-=32;
+        }
+    }
+    cout<<str;
+    return(0);
+}
